2cf57b8b44b6383228a6159f5db4dd6779db5e04
putnamn21 <nathanputnam2@gmail.com>
2022-01-21 12:38:42 -0600

    dummy commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+
+//this is pointless :)
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
